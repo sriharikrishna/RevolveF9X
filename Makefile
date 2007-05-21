67328cc53785b0bbc73e998a33b3ea482b308976
m_utkej <m_utkej@utke2.mcs.anl.gov>
2007-05-21 13:12:03 -0500

    baseline

---
+++ Makefile
@@ -0,0 +1,17 @@
+ifndef F90C
+F90C=gfortran
+endif
+
+ifndef F90CFLAGS
+F90CFLAGS=
+endif
+
+example : revolve.o example.f90
+	$(F90C) $(F90CFLAGS) -o $@ $^
+
+%.o: %.f90
+	$(F90C) $(F90CFLAGS) -c -o $@ $<
+
+clean: 
+	rm -f *.o *.mod example
+
---
+++ example.f90
@@ -0,0 +1,32 @@
+PROGRAM example
+  USE revolve
+  IMPLICIT NONE
+  INTEGER :: checkpoints,steps, guessedCheckpoints,verbosity=0
+  TYPE(rvAction) :: theAction
+  LOGICAL :: initialized=.FALSE.
+  CHARACTER , DIMENSION(80) :: errorMsg
+  DO WHILE (verbosity<1 .OR. verbosity >3)
+     WRITE (*,FMT='(A)',ADVANCE='NO') 'verbosity level (1=summary, 2=checkpoints, 3=all actions)? :'
+     READ (*,*) verbosity
+     if (verbosity<1 .OR. verbosity >3) THEN 
+        WRITE(*,*) 'invalid entry, try again'
+     ELSE
+        CALL rvVerbose(verbosity)
+     ENDIF
+  END DO
+  DO WHILE (.NOT.initialized)
+     WRITE (*,FMT='(A)',ADVANCE='NO') 'number of timesteps? :'
+     READ (*,*) steps
+     guessedCheckPoints=rvGuess(steps)
+     WRITE (*,'(A,I8,A,F8.4)') 'revolve suggests :', guessedCheckPoints, ' checkpoints for a factor of ', &
+          rvFactor(steps,guessedCheckPoints)
+     WRITE (*,FMT='(A)',ADVANCE='NO') 'how many checkpoints are allowed? :'
+     READ (*,*) checkpoints
+     initialized=rvInit(steps,checkpoints,errorMsg)
+     IF (.NOT.initialized) WRITE(*,'(A,A)') 'Error: ', errorMsg
+  END DO
+  if (verbosity>1) WRITE(*,'(A)') 'actions:'
+  DO WHILE (theAction%actionFlag/=rvDone)
+     theAction=rvNextAction()
+  END DO
+END PROGRAM example
