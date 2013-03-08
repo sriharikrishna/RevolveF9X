1802d7c0c743680d5c078593e6dc2acd6aafe0fa
utke on macbook <utke on macbook@localhost>
2013-03-07 18:10:28 -0600

    separate out examples into a separate directory

--- Examples/MakeCommon.mk
+++ Examples/MakeCommon.mk
@@ -6,10 +6,13 @@ ifndef F90CFLAGS
 F90CFLAGS=-O3
 endif
 
-all : revolve.o 
+all : example
+
+example: example.o ../../revolve.o
+	$(F90C) $(F90CFLAGS) -o $@ $^
 
 %.o: %.f90
-	$(F90C) $(F90CFLAGS) -c -o $@ $<
+	$(F90C) $(F90CFLAGS) -I../../ -c -o $@ $<
 
 clean: 
 	rm -f *.o *.mod example
